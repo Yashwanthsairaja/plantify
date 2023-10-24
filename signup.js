424f9c161a691ddd2ded258924cfe64a32f55aad
Yashwanthsairaja <148843002+Yashwanthsairaja@users.noreply.github.com>
2023-10-24 07:35:58 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -4,3 +4,11 @@ const signUpBtn = document.querySelector(".green-bg button");
 signUpBtn.addEventListener("click", () => {
   container.classList.toggle("change");
 });
+// Select the "Sign Up" button by its id
+const signupButton = document.getElementById("signupButton");
+
+// Add a click event listener to the button
+signupButton.addEventListener("click", () => {
+  // Redirect to the dashboard.html page
+  window.location.href = "dashboard.html";
+});
