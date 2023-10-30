253a92cb3051f47e96790452551a0e6895d505d5
Yashwanthsairaja <148843002+Yashwanthsairaja@users.noreply.github.com>
2023-10-29 20:33:19 -0400

    Update myplants.js

--- myplants.js
+++ myplants.js
@@ -1,49 +1,51 @@
-// Sample plant data (you can replace this with your data)
-const plantData = [
-    {
-        name: "Spider Plant",
-        image: "plant1.jpg",
-        status: "Healthy",
-        wateringDue: "2 days",
-        fertilizerDue: "5 days",
-    },
-    {
-        name: "Aloe Vera",
-        image: "plant2.jpg",
-        status: "Healthy",
-        wateringDue: "3 days",
-        fertilizerDue: "6 days",
-    },
-    {
-        name: "Fern",
-        image: "plant3.jpg",
-        status: "Infection",
-        wateringDue: "1 day",
-        fertilizerDue: "4 days",
-    },
-    // Add more plant data here
-];
+document.addEventListener("DOMContentLoaded", function() {
+    // Sample plant data (you can replace this with your data)
+    const plantData = [
+        {
+            name: "Spider Plant",
+            image: "plant1.jpg",
+            status: "Healthy",
+            wateringDue: "2 days",
+            fertilizerDue: "5 days",
+        },
+        {
+            name: "Aloe Vera",
+            image: "plant2.jpg",
+            status: "Healthy",
+            wateringDue: "3 days",
+            fertilizerDue: "6 days",
+        },
+        {
+            name: "Fern",
+            image: "plant3.jpg",
+            status: "Infection",
+            wateringDue: "1 day",
+            fertilizerDue: "4 days",
+        },
+        // Add more plant data here
+    ];
 
-// Function to create plant cards
-function createPlantCards() {
-    const plantCards = document.querySelector(".plant-cards");
+    // Function to create plant cards
+    function createPlantCards() {
+        const plantCards = document.getElementById("plant-cards");
 
-    plantData.forEach((plant) => {
-        const card = document.createElement("div");
-        card.classList.add("plant-card");
-        card.innerHTML = `
-            <img src="${plant.image}" alt="${plant.name}">
-            <div class="info">
-                <h3>${plant.name}</h3>
-                <p>Status: ${plant.status}</p>
-                <p>Watering due in: ${plant.wateringDue}</p>
-                <p>Fertilizing due in: ${plant.fertilizerDue}</p>
-            </div>
-        `;
+        plantData.forEach((plant) => {
+            const card = document.createElement("div");
+            card.classList.add("plant-card");
+            card.innerHTML = `
+                <img src="${plant.image}" alt="${plant.name}">
+                <div class="info">
+                    <h3>${plant.name}</h3>
+                    <p>Status: <span class="badge ${plant.status.toLowerCase()}">${plant.status}</span></p>
+                    <p>Watering due in: ${plant.wateringDue}</p>
+                    <p>Fertilizing due in: ${plant.fertilizerDue}</p>
+                </div>
+            `;
 
-        plantCards.appendChild(card);
-    });
-}
+            plantCards.appendChild(card);
+        });
+    }
 
-// Call the function to create plant cards when the page loads
-window.onload = createPlantCards;
+    // Call the function to create plant cards when the page loads
+    createPlantCards();
+});
