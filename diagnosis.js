aca3e6885645008d0fca77fbb62ca68a68027d81
Yashwanthsairaja <148843002+Yashwanthsairaja@users.noreply.github.com>
2023-11-05 03:10:03 -0500

    Update diagnosis.js

--- diagnosis.js
+++ diagnosis.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const diagnoseButton = document.getElementById("diagnose-btn");
     const diagnosisReport = document.getElementById("diagnosis-report");
+    const downloadReport = document.getElementById("download-report");
 
     diagnoseButton.addEventListener("click", function() {
         const plantImage = document.getElementById("plant-image").files[0];
@@ -9,9 +10,14 @@ document.addEventListener("DOMContentLoaded", function() {
         // In a real application, you'd need to send the image and description to a server for processing.
         // For this example, we'll just show a placeholder message.
 
+        diagnosisReport.style.display = "block";
         diagnosisReport.innerHTML = "<h2>Diagnosis Report</h2>" +
             "<p>Image and description sent for processing.</p>" +
             "<p>Awaiting results...</p>";
+
+        // Simulate a delay before showing the download message
+        setTimeout(function() {
+            downloadReport.style.display = "block";
+        }, 4000);
     });
 });
-
