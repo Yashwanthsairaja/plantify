2e1bb792ebc0e1114ba15b1c7bb4ffa8a427baf3
Yashwanthsairaja <148843002+Yashwanthsairaja@users.noreply.github.com>
2023-11-05 04:54:53 -0500

    Update community.js

--- community.js
+++ community.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
-    $('#createPostForm').submit(function(e) {
-        e.preventDefault();
+    // Sample data to simulate posts (you can replace this with actual data)
+    const samplePosts = [
+        { title: 'New Plant Enthusiast - Greetings to the Community!', content: "Hey everyone, I'm a new plant enthusiast...", },
+        { title: 'Sharing My Blooming Succulent Collection', content: "Hi Plantify community! I just wanted to share the joy...", }
+    ];
 
-        const postTitle = $('#postTitle').val();
-        const postContent = $('#postContent').val();
-
-        const newPost = `
+    // Function to create and append a new post to the post list
+    function appendPost(title, content) {
+        const postHTML = `
             <div class="col-md-6">
                 <div class="card mb-4">
                     <div class="card-body">
-                        <h5 class="
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${content}</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        $('#post-list .row').append(postHTML);
+    }
+
+    // Initial posts on page load (you can replace this with your data)
+    samplePosts.forEach(post => {
+        appendPost(post.title, post.content);
+    });
+
+    $('#createPostForm').submit(function(e) {
+        e.preventDefault();
+        const postTitle = $('#postTitle').val();
+        const postContent = $('#postContent').val();
+        // Add the new post to the page
+        appendPost(postTitle, postContent);
+        // Reset form fields
+        $('#postTitle').val('');
+        $('#postContent').val('');
+    });
+});
