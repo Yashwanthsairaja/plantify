22ac187a48a0317202007e0e4ae29a066dd25013
Yashwanthsairaja <148843002+Yashwanthsairaja@users.noreply.github.com>
2023-11-05 00:36:34 -0400

    Update addplant.js

--- addplant.js
+++ addplant.js
@@ -4,27 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     addPlantForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
-        // Get form data
-        const plantName = document.getElementById("plant-name").value;
-        const wateringNeeds = document.getElementById("watering-need").value;
-        const sunlightNeeds = document.getElementById("sunlight-need").value;
-        const fertilizerNeeds = document.getElementById("fertilizer-need").value;
-        const placement = document.getElementById("placement").value;
-        const plantHeight = document.getElementById("height").value;
-
-        // Add the plant data to your collection (in-memory array)
-        // You can replace this with your data storage mechanism (e.g., a database)
-        const newPlant = {
-            name: plantName,
-            watering: wateringNeeds,
-            sunlight: sunlightNeeds,
-            fertilizer: fertilizerNeeds,
-            placement: placement,
-            height: plantHeight,
-        };
+        // Get form data and handle it (e.g., store in a database).
+        // ...
 
         // Redirect to the confirmation page
         window.location.href = "confirmation.html";
     });
 });
-
